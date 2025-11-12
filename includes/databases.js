83e2bae91855d1ebdb1feeb43f90ef9abd5c20b9
Federico Campolongo <fcampolongo@nowvertical-es.com>
2025-11-12 14:22:02 +0000

    prueba

---
+++ includes/databases.js
@@ -0,0 +1,6 @@
+module.exports = {
+    raw: "logrand-storage-raw-dev",  // Solo el nombre de la base de datos
+    stage: "logrand-storage-staging-dev",
+    analytics: "logrand-storage-analytics-dev",
+    computo: "logrand-computo-dev"
+}
