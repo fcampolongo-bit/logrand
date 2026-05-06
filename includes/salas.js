4eb2a7c4637dcb69cf40cf1ccba1e11338906fd8
Federico Campolongo <fcampolongo@nowvertical-es.com>
2026-05-06 21:03:08 +0000

    pasaje

--- includes/salas.js
+++ includes/salas.js
@@ -1,5 +1,8 @@
 const lista_salas = [
-    { esquema: "STG_WIGOS_JUBILEE", nombre: "JUBILEE", id: 95, offset: 7 }
+    /*{ esquema: "STG_WIGOS_JUBILEE", nombre: "JUBILEE", id: 95, offset: 7 }*/
+    { esquema: "STG_WIGOS_JUBILEE", nombre: "JUBILEE", id: 95 },
+     { esquema: "STG_WIGOS_CONSTITUCION", nombre: "CONSTITUCION", id: 99 },
+     { esquema: "STG_WIGOS_DUBAI", nombre: "DUBAI", id: 93 }
 ];
 
 module.exports = { lista_salas };
