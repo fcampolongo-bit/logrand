4deedd9b35e6e63569cd50d53183fa7ea18547b0
Federico Campolongo <fcampolongo@nowvertical-es.com>
2026-05-13 15:32:11 +0000

    pasaje

--- includes/salas.js
+++ includes/salas.js
@@ -4,8 +4,11 @@ const lista_salas = [
      { esquema: "STG_WIGOS_CONSTITUCION", nombre: "CONSTITUCION", id: 99 },
      { esquema: "STG_WIGOS_DUBAI", nombre: "DUBAI", id: 93 },
     { esquema: "STG_WIGOS_ESTANZUELA", nombre: "ESTANZUELA", id: 98 },
-    { esquema: "STG_WIGOS_GRANDJUBILEE", nombre: "JUBILEEGRANDCASINO", id: 288 } 
-       
+    { esquema: "STG_WIGOS_GRANDJUBILEE", nombre: "JUBILEEGRANDCASINO", id: 288 },
+    { esquema: "STG_WIGOS_PARADISE", nombre: "PARADISE", id: 89 } ,
+    { esquema: "STG_WIGOS_TAJMAHAL", nombre: "TAJMAHAL", id: 94 } ,
+    { esquema: "STG_WIGOS_VIVAMEXICO", nombre: "VIVAMEXICO", id: 7 },
+     {esquema: "STG_WIGOS_NEWYORK", nombre: "NEWYORK", id: 90 }
 ];
 
 module.exports = { lista_salas };
